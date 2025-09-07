050d8c300a57196271828c880626c5dac05c1f62
Zay Lin Htet <70065913+ZayLin799@users.noreply.github.com>
2025-09-07 15:50:39 +0700

    change

--- components/About.jsx
+++ components/About.jsx
@@ -1,35 +1,41 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import AboutImg from '../public/assets/about.jpg';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import AboutImg from "../public/assets/about.jpg";
 
 const About = () => {
   return (
-    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
-      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
-        <div className='col-span-2'>
-          <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
+    <div id="about" className="flex items-center w-full p-2 py-16 md:h-screen">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5]">
             About
           </p>
-          <h2 className='py-4'>Who I Am</h2>
-          <p className='py-2 text-gray-600'>
-            Highly motivated and results-oriented full-stack developer with 3 years of hands-on
-            experience in conceptualizing, designing, developing, and implementing full-stack
-            applications across diverse business domains. Possess a comprehensive
-            understanding of both front-end and back-end technologies, allowing me to
-            successfully bridge the gap between user-facing requirements and robust, scalable
-            server-side solutions. I excel at working both independently on clearly defined
-            projects and collaboratively within cross-functional teams - adapting my approach to 
-            ensure project goals are met on time and to the highest quality standards.
+          <h2 className="py-4">Who I Am</h2>
+          <p className="py-2 text-gray-600">
+            A highly motivated and adaptable Sofware Developer with 4+ years of
+            experience designing and implementing reliable, scalable, and secure
+            web applications. Proficient in backend technologies including
+            Node.js, Nest.js, Laravel (PHP) , and database design, with
+            expertise in RESTful APIs, authentication/authorization, and cloud
+            deployment . Adept at building high-performance backend systems that
+            drive user-centric solutions across diverse industries. Additionally
+            skilled in Next.js and React for developing modern, responsive
+            front-end interfaces when needed. Experienced working in fast-paced,
+            multicultural teams, applying agile best practices, and writing
+            clean, maintainable code. Strong problem-solving mindset with a
+            proven ability to optimize performance, ensure system reliability,
+            and deliver high-quality results across all stages of the software
+            development lifecycle.
           </p>
-          <Link href='/#projects'>
-            <p className='py-2 text-gray-600 underline cursor-pointer'>
+          <Link href="/#projects">
+            <p className="py-2 text-gray-600 underline cursor-pointer">
               Check out some of my latest projects.
             </p>
           </Link>
         </div>
-        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <Image src={AboutImg} className='rounded-xl' alt='/' />
+        <div className="flex items-center justify-center w-full h-auto p-4 m-auto duration-300 ease-in shadow-xl shadow-gray-400 rounded-xl hover:scale-105">
+          <Image src={AboutImg} className="rounded-xl" alt="/" />
         </div>
       </div>
     </div>
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,52 +1,52 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiOutlineMail } from 'react-icons/ai';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
-import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import Link from "next/link";
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Main = () => {
   return (
-    <div id='home' className='w-full h-screen text-center'>
-      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+    <div id="home" className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <p className='text-sm tracking-widest text-gray-600 uppercase'>
+          <p className="text-sm tracking-widest text-gray-600 uppercase">
             LET&#39;S BUILD SOMETHING TOGETHER
           </p>
-          <h1 className='py-4 text-gray-700'>
-            Hi, I&#39;m <span className='text-[#5651e5]'> Zay Lin Htet</span>
+          <h1 className="py-4 text-gray-700">
+            Hi, I&#39;m <span className="text-[#5651e5]"> Zay Lin Htet</span>
           </h1>
-          <h1 className='py-2 text-gray-700'>A Full Stack Software Developer</h1>
-          <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
-            I’m focused on building responsive front-end web applications
-            integrating back-end technologies.
+          <h1 className="py-2 text-gray-700">A Software Developer</h1>
+          <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
+            I’m a Software Developer passionate about building reliable,
+            scalable, and user-centric web applications.
           </p>
-          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <a
-              href='https://www.linkedin.com/in/zay-lin-htet/'
-              target='_blank'
-              rel='noreferrer'
+              href="https://www.linkedin.com/in/zay-lin-htet/"
+              target="_blank"
+              rel="noreferrer"
             >
-              <div className='p-6 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110'>
+              <div className="p-6 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110">
                 <FaLinkedinIn />
               </div>
             </a>
             <a
-              href='https://github.com/ZayLin799'
-              target='_blank'
-              rel='noreferrer'
+              href="https://github.com/ZayLin799"
+              target="_blank"
+              rel="noreferrer"
             >
-              <div className='p-6 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110'>
+              <div className="p-6 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110">
                 <FaGithub />
               </div>
             </a>
-            <Link href='/#contact'>
-              <div className='p-6 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110'>
+            <Link href="/#contact">
+              <div className="p-6 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110">
                 <AiOutlineMail />
               </div>
             </Link>
             <Link href="/Zay-Lin-Htet-CV.pdf" passHref>
               <a target="_blank">
-                <div className='p-6 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110'>
+                <div className="p-6 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110">
                   <BsFillPersonLinesFill />
                 </div>
               </a>
