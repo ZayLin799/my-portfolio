3b3b263798fd4968e82e6f3e24ff052eb7ce4aad
Zay Lin Htet <70065913+ZayLin799@users.noreply.github.com>
2025-09-07 15:38:50 +0700

    Add JP Auction Sheet project and update UI

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,52 +1,59 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import Viabells from '../public/assets/projects/viabells.png';
-import LilImg from '../public/assets/projects/little-myanmar.jpg';
-import Sdax from '../public/assets/projects/sdax.png';
-import AmSKin from '../public/assets/projects/am-skin.png';
-import ProjectItem from './ProjectItem';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Viabells from "../public/assets/projects/viabells.png";
+import LilImg from "../public/assets/projects/little-myanmar.jpg";
+import Sdax from "../public/assets/projects/sdax.png";
+import AmSKin from "../public/assets/projects/am-skin.png";
+import JPauctionsheet from "../public/assets/projects/jp-auctionsheet.png";
+import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
   return (
-    <div id='projects' className='w-full'>
-      <div className='max-w-[1240px] mx-auto px-2 py-16'>
-        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           OutSource Projects
         </p>
-        <h2 className='py-4'>What I&apos;ve Built</h2>
-        <div className='grid md:grid-cols-2 gap-8'>
+        <h2 className="py-4">What I&apos;ve Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
           <ProjectItem
-            title='Little Myanmar'
+            title="Little Myanmar"
             backgroundImg={LilImg}
-            projectUrl='/littleMyanmar'
-            tech='Laravel'
+            projectUrl="/littleMyanmar"
+            tech="Laravel"
+          />
+          <ProjectItem
+            title="JP Auction Sheet"
+            backgroundImg={JPauctionsheet}
+            projectUrl="/JPAuctionSheet"
+            tech="Laravel"
           />
         </div>
       </div>
-      <div className='max-w-[1240px] mx-auto px-2 py-16'>
-        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Fulltime job Projects
         </p>
-        <h2 className='py-4'>What I&apos;ve Paticipated</h2>
-        <div className='grid md:grid-cols-2 gap-8'>
+        <h2 className="py-4">What I&apos;ve Paticipated</h2>
+        <div className="grid md:grid-cols-2 gap-8">
           <ProjectItem
-            title='Sdax'
+            title="Sdax"
             backgroundImg={Sdax}
-            projectUrl='/sdax'
-            tech='React / Java (Spring WebFlux)'
+            projectUrl="/sdax"
+            tech="React / Java (Spring WebFlux)"
           />
           <ProjectItem
-            title='Viabells'
+            title="Viabells"
             backgroundImg={Viabells}
-            projectUrl='/viabell'
-            tech='Next.js'
+            projectUrl="/viabell"
+            tech="Next.js"
           />
           <ProjectItem
-            title='A.M Skincare'
+            title="A.M Skincare"
             backgroundImg={AmSKin}
-            projectUrl='/amSkin'
-            tech='Wordpress / PHP'
+            projectUrl="/amSkin"
+            tech="Wordpress / PHP"
           />
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,29 @@
-import Head from 'next/head'
-import About from '../components/About'
-import Contact from '../components/Contact'
-import Main from '../components/Main'
-import Projects from '../components/Projects'
-import Skills from '../components/Skills'
-
+import Head from "next/head";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Main from "../components/Main";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
-        <title>Zay Lin Htet | Full Stack Developer</title>
-        <meta name="description" content="I’m a full stack developer specializing in building web application and mobile application." />
+        <title>Zay Lin Htet | Software Developer</title>
+        <meta
+          name="description"
+          content="Software Developer with experience building reliable, scalable, and user-focused web applications."
+        />
         <link rel="icon" href="/fav.png" />
       </Head>
-    <Main />
-    <About />
-    <Skills />
-    <Projects />
-    <Contact />
-    </div>
-  )
+
+      <main>
+        <Main />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
+    </>
+  );
 }
